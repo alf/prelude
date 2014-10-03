f65e06ddbd2f4099beaed417642b278079b6b67d
Alf Lervag <alf.lervag@bouvet.no>
2014-10-03 10:14:40 +0200

    Tweaked my org-mode setup

--- personal/setup-org.el
+++ personal/setup-org.el
@@ -18,46 +18,52 @@
 (define-key alf/ctl-z-map "j" 'org-clock-goto)
 (define-key alf/ctl-z-map (kbd  "SPC") 'bh/clock-in-last-task)
 
-
+;; Setup org-mobile
 (setq org-mobile-inbox-for-pull "~/Dropbox/Org/refile.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 
+;; Store my org-mode files in dropbox
 (setq dropbox-dir (expand-file-name "~/Dropbox"))
 (setq org-directory (file-name-as-directory
                      (expand-file-name "Org" dropbox-dir)))
 
+;; Make expand-file-name operate in my org-directory
 (let ((default-directory org-directory))
   (setq org-agenda-files
         (mapcar 'expand-file-name
-                (list "refile.org"
-                      "todo.org"
-                      "personal.org"
-                      "journal.org"
-                      "projects")))
+                (list "refile.org" ; Captured items go here
+                      "todo.org" ; General tasks outside of projects
+                      "work.org" ; Notes and tasks for work
+                      "personal.org" ; Personal items
+                      "journal.org" ; This is where I log thoughts and what I do
+                      "projects"))) ; This folder contains my project files
 
+  ;; This causes org-capture to store things in this file
   (setq org-default-notes-file (expand-file-name "refile.org"))
 
+  ;; My various capture templates
   (setq org-capture-templates
         `(("t" "todo" entry (file ,(expand-file-name "refile.org"))
-           "* TODO %?\n	%i\n	%a" :clock-in t :clock-resume t)
+           "* TODO %?\n\t%a\n%i" :clock-in t :clock-resume t)
           ("T" "todo under current clock" entry (clock)
-           "* TODO %?\n	%i\n	%a" )
+           "* TODO %?\n\t%a\n%i" )
           ("c" "capture" entry (file ,(expand-file-name "refile.org"))
-           "* %?\n	%i\n	%a" :clock-in t :clock-resume t)
+           "* %?\n\t%a\n%i" :clock-in t :clock-resume t)
           ("n" "note" entry (file ,(expand-file-name "refile.org"))
-           "* %? :NOTE:\n	%a" :clock-in t :clock-resume t)
+           "* %? :NOTE:\n\t%a" :clock-in t :clock-resume t)
           ("i" "Interruptionion" entry (file ,(expand-file-name "refile.org"))
-           "* %? :INTERRUPTION:\n	%i\n	%a" :clock-in t :clock-resume t)
+           "* %? :INTERRUPTION:\n\t%a\n%i" :clock-in t :clock-resume t)
           ("j" "Journal" entry (file+datetree ,(expand-file-name "journal.org"))
-           "* %?\n	%i\n	%a" :clock-in t :clock-resume t)
+           "* %?\n\t%a\n%i" :clock-in t :clock-resume t)
           ("b" "Blog idea" entry (file+headline ,(expand-file-name "blog-ideas.org") "Blog ideas")
            "** %?\n%u\n")
           ("r" "respond" entry (file ,(expand-file-name "refile.org"))
-           "* TODO Respond to %:from on %:subject\n	SCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t))))
+           "* TODO Respond to %:from on %:subject\n\tSCHEDULED: %t\n%U\n\t%a\n" :clock-in t :clock-resume t :immediate-finish t))))
 
+;; My workflow
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "|" "DONE(d)")
-              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE"))))
+      (quote ((sequence "TODO(t/!)" "STARTED(s/!)" "WAITING(w@/!)" "|" "DONE(d/!)")
+              (sequence "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE"))))
 
 (setq org-todo-keyword-faces
       (quote (("TODO" :foreground "red" :weight bold)
@@ -120,6 +126,95 @@
 ;; Don't bother showing the past, the future is more interesting
 ;; We use reporting for the past
 (setq org-agenda-start-on-weekday nil)
+
+;; When at the beginning of a headline, speed commands allow for quick navigation
+(setq org-use-speed-commands t)
+
+;; Don't inherit the PROJECT tag since subtasks of a project should
+;; not be considered a project
+(add-to-list 'org-tags-exclude-from-inheritance "PROJECT")
+
+;; Resume clocking task when emacs is restarted
+(org-clock-persistence-insinuate)
+;; Show lot of clocking history so it's easy to pick items off the C-F11 list
+(setq org-clock-history-length 23)
+;; Resume clocking task on clock-in if the clock is open
+(setq org-clock-in-resume t)
+;; Save clock data and state changes and notes in the LOGBOOK drawer
+(setq org-clock-into-drawer t)
+;; Clock out when moving task to a done state
+(setq org-clock-out-when-done t)
+;; Save the running clock and all clock history when exiting Emacs, load it on startup
+(setq org-clock-persist t)
+;; Do not prompt to resume an active clock
+(setq org-clock-persist-query-resume nil)
+;; Enable auto clock resolution for finding open clocks
+(setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
+;; Include current clocking task in clock reports
+(setq org-clock-report-include-clocking-task t)
+
+;; Org-mode should be smarter in headlines
+(setq org-special-ctrl-a/e t)
+(setq org-special-ctrl-k t)
+
+
+;; Write a small message when marking something as done
+(setq org-log-done 'note)
+(setq org-log-into-drawer t)
+
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+
+;; Use full outline paths for refile targets - makes it easier with helm
+(setq org-refile-use-outline-path t)
+;; Allow refile to create parent tasks with confirmation
+(setq org-refile-allow-creating-parent-nodes (quote confirm))
+(setq org-outline-path-complete-in-steps nil)
+(setq org-completion-use-helm t)
+
+(setq org-agenda-clock-consistency-checks
+      (quote (:max-duration "8:00"
+                            :min-duration 0
+                            :max-gap 0
+                            :gap-ok-around ("4:00"))))
+
+;; global Effort estimate values
+;; global STYLE property values for completion
+(setq org-global-properties
+      (quote (("Effort_ALL" . "0:15 0:30 1:00 2:00 4:00 8:00"))))
+
+(setq org-archive-location "%s_archive::* Archived Tasks")
+
+(setq org-time-clocksum-format
+      '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+
+(setq org-agenda-clockreport-parameter-plist
+      (quote (:link t :maxlevel 5 :fileskip0 t :narrow 80)))
+
+
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (dot . t)
+         (ditaa . t)
+         (python . t)
+         (gnuplot . t)
+         (clojure . t)
+         (sh . t)
+         (plantuml . t)
+         (latex . t))))
+
+(setq org-startup-with-inline-images t)
+
+
+;; Include agenda archive files when searching for things
+(setq org-agenda-text-search-extra-files (quote (agenda-archives)))
+
+(setq org-read-date-prefer-future 'time)
+
+(setq org-src-fontify-natively t)
+
 
 (defun alf/org-punch-dwim ()
   (interactive)
