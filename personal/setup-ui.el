2316ff91875d68626bc268700b1d6b0be502db02
Alf Lervag <alf.lervag@bouvet.no>
2014-10-03 10:14:30 +0200

    I want larger fonts on bigger screens

--- personal/setup-ui.el
+++ personal/setup-ui.el
@@ -8,5 +8,9 @@
 
 (setq visible-bell t)
 
+(if (> (display-pixel-width) 1440)
+    (set-face-attribute 'default nil :height 200)
+  (set-face-attribute 'default nil :height 140))
+
 (provide 'setup-ui)
 ;;; setup-ui.el ends here
