6b29a09626720a35f07cc656ac638bd5030f0c90
Alf Lervag <alf.lervag@bouvet.no>
2014-10-03 16:26:04 +0200

    Added ggtags and ag
    These tools are amazing!

---
+++ personal/setup-editor.el
@@ -0,0 +1,41 @@
+;;; setup-editor.el --- editor customizations
+
+;;
+;; Copyright © 2014 Alf Lervåg
+;;
+;; Author: Alf Lervåg <alf@lervag.net>
+;; URL: https://github.com/alf/prelude
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Refinements of the core editing experience in Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(prelude-require-package 'ggtags)
+(prelude-require-package 'ag)
+
+(provide 'setup-editor)
+
+;;; setup-editor.el ends here
