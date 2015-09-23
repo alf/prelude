6aa4d2eb48130a5bb39398a3bb21823e92cdfa9f
Alf Lervag <alf.lervag@bouvet.no>
2015-09-23 09:53:04 +0200

    Tweaked my ui

--- personal/setup-ui.el
+++ personal/setup-ui.el
@@ -7,17 +7,41 @@
 ;;; Code:
 
 (setq visible-bell t)
+(scroll-bar-mode -1)
 
 (if (> (display-pixel-width) 1440)
-    (set-face-attribute 'default nil :height 200)
-  (set-face-attribute 'default nil :height 140))
+    (set-face-attribute 'default nil :height 140)
+  (set-face-attribute 'default nil :height 120))
 
-(require 'helm)
-(setq helm-split-window-default-side 'right)
-(setq helm-split-window-in-side-p nil)
+(defun window-toggle-split-direction ()
+  "Switch window split from horizontally to vertically, or vice versa.
 
-;; Prefer eww when opening urls
-(setq browse-url-browser-function 'eww-browse-url)
+i.e. change right window to bottom, or change bottom window to right."
+  (interactive)
+  (require 'windmove)
+  (let ((done))
+    (dolist (dirs '((right . down) (down . right)))
+      (unless done
+        (let* ((win (selected-window))
+               (nextdir (car dirs))
+               (neighbour-dir (cdr dirs))
+               (next-win (windmove-find-other-window nextdir win))
+               (neighbour1 (windmove-find-other-window neighbour-dir win))
+               (neighbour2 (if next-win (with-selected-window next-win
+                                          (windmove-find-other-window neighbour-dir next-win)))))
+          ;;(message "win: %s\nnext-win: %s\nneighbour1: %s\nneighbour2:%s" win next-win neighbour1 neighbour2)
+          (setq done (and (eq neighbour1 neighbour2)
+                          (not (eq (minibuffer-window) next-win))))
+          (if done
+              (let* ((other-buf (window-buffer next-win)))
+                (delete-window next-win)
+                (if (eq nextdir 'right)
+                    (split-window-vertically)
+                  (split-window-horizontally))
+                (set-window-buffer (windmove-find-other-window neighbour-dir) other-buf))))))))
+
+(prelude-require-package 'highlight-symbol)
+(highlight-symbol-mode t)
 
 (provide 'setup-ui)
 ;;; setup-ui.el ends here
